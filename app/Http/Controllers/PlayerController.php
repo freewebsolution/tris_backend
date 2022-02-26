49942cfd9eb2ec9c77d384b4c25d09c2c807de70
Lucio <info@freewebsolution.it>
2022-02-26 11:57:48 +0100

    Crea Api Route, middleware cors e testa api

---
+++ app/Http/Controllers/PlayerController.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Player;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Response;
+
+class PlayerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $players = Player::all();
+        $response = Response::json($players,200);
+        return $response;
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        if ((!$request->name) || (!$request->email)) {
+
+            $response = Response::json([
+                'message' => 'Please enter all required fields'
+            ], 422);
+            return $response;
+        }
+
+        $player = new Player(array(
+            'name' => trim($request->name),
+            'email' => trim($request->email),
+        ));
+        $player->save();
+
+        $message = 'Your player has been added successfully';
+
+        $response = Response::json([
+            'message' => $message,
+            'data' => $player,
+        ], 201);
+
+        return $response;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Player  $player
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Player $player)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Player  $player
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Player $player)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Player  $player
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Player $player)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Player  $player
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Player $player)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::group(['prefix' => 'v1', 'middleware' => 'cors'], function(){
+    Route::resource('players', \App\Http\Controllers\PlayerController::class);
+});
